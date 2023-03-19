211ec204f9d2be64a89ce5375f2f74905b39a7cd
Wesley <wesleyhoo@hotmail.com>
2023-03-19 06:49:06 -0300

    Add countdown list

--- main.js
+++ main.js
@@ -11,9 +11,27 @@ function sumUp() {
 }
 
 
-let badgeImg = document.getElementById('team-badge');
-let team = document.querySelectorAll('option');
+// let badgeImg = document.getElementById('team-badge');
+// let team = document.querySelectorAll('option');
 
-team.addEventListener('onchange', () => {
-    badgeImg.style.display = 'block';
-})
+// team.addEventListener('onchange', () => {
+//     badgeImg.style.display = 'block';
+// })
+
+let numbers = document.getElementById('countdown');
+numbers.innerHTML = '';
+
+for(let i = 10; i >= 0; i--) {
+    const para = document.createElement('p');
+    para.textContent = i - 1;
+    numbers.appendChild(para);
+    
+    if(i === 10) {
+        para.textContent = 'Countdown 10';
+    } else if( i === 0) {
+        para.textContent = 'Blast off!'
+    } else {
+        para.textContent = i;
+    }
+    
+}
